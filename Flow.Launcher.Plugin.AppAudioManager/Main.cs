96268f367ec931453221fcfa8b9c674e99fd39ac
David Brett <davidbrett2002@gmail.com>
2026-01-04 10:36:47 +0100

    list all audio sessions and toggle mute

--- Flow.Launcher.Plugin.AppAudioManager/Main.cs
+++ Flow.Launcher.Plugin.AppAudioManager/Main.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using Flow.Launcher.Plugin;
+using NAudio.CoreAudioApi;
+using System.Diagnostics;
 
 namespace Flow.Launcher.Plugin.AppAudioManager
 {
@@ -8,14 +10,69 @@ namespace Flow.Launcher.Plugin.AppAudioManager
     {
         private PluginInitContext _context;
 
+        private MMDeviceEnumerator deviceEnumerator;
+
         public void Init(PluginInitContext context)
         {
             _context = context;
+
+            deviceEnumerator = new MMDeviceEnumerator();
+
         }
 
         public List<Result> Query(Query query)
         {
-            return new List<Result>();
+            var results = new List<Result>();
+
+            MMDeviceCollection audioDeviceEndpoints = deviceEnumerator.EnumerateAudioEndPoints(
+                DataFlow.Render, // Output devices
+                DeviceState.Active
+            );
+
+            foreach (var device in audioDeviceEndpoints)
+            {
+
+                var sessions = device.AudioSessionManager.Sessions;
+
+                for (int i = 0; i < sessions.Count; i++)
+                {
+                    AudioSessionControl session = sessions[i];
+
+
+                    // Get session state: Inactive, Active, or Expired
+                    var state = session.State;
+                    
+                    // Try to get process info
+                    uint processId = session.GetProcessID;
+                    string processName = "Unknown";
+                    string mainWindowTitle = "";
+                    try
+                    {
+                        var process = Process.GetProcessById((int)processId);
+                        processName = process.ProcessName;
+                        mainWindowTitle = process.MainWindowTitle;
+                        
+                    }
+                    catch (ArgumentException)
+                    {
+                        // Process may have exited or PID is invalid
+                    }
+
+                    results.Add(new Result{
+                        Title = session.DisplayName,
+                        SubTitle = $"{processId} - {state} - {processName} - {mainWindowTitle}",
+                        IcoPath = session.IconPath,
+                        Action = _ =>
+                        {
+                            // Toggle mute
+                            session.SimpleAudioVolume.Mute = !session.SimpleAudioVolume.Mute;
+                            return true;
+                        }
+                    });
+                }
+            }
+
+            return results;
         }
     }
 }
