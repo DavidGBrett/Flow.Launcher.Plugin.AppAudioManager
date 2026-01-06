b537d682b5d56fb31b8c228ee0ff843edf26f4dd
David Brett <davidbrett2002@gmail.com>
2026-01-06 13:22:48 +0100

    Add Builder to Action Option

--- Flow.Launcher.Plugin.AppAudioManager/ActionOption.cs
+++ Flow.Launcher.Plugin.AppAudioManager/ActionOption.cs
@@ -4,9 +4,45 @@ using System.Collections.Generic;
 namespace Flow.Launcher.Plugin.AppAudioManager
 {
     public record ActionOption(
-        List<string> Names,
         Func<Result> CreateResult,
+        List<string> Names = null,
         string SubOptionKeyword = null,
         Func<List<ActionOption>> GetSubOptions = null
-    );
+    ){
+        public class Builder
+        {
+            private Func<Result> createResult;
+            private List<string> names;
+            private string subOptionKeyword;
+            private Func<List<ActionOption>> getSubOptions;
+
+            public Builder(Func<Result> CreateResult)
+            {
+                createResult = CreateResult;
+            }
+
+            public Builder WithNames(List<string> names)
+            {
+                this.names = names;
+                return this;
+            }
+            public Builder WithSubOptions(string subOptionKeyword, Func<List<ActionOption>> getSubOptions)
+            {
+                this.subOptionKeyword = subOptionKeyword;
+                this.getSubOptions = getSubOptions;
+                return this;
+            }
+
+            public ActionOption Build()
+            {
+                return new ActionOption(
+
+                    CreateResult: createResult,
+                    Names: names,
+                    SubOptionKeyword: subOptionKeyword,
+                    GetSubOptions: getSubOptions
+                );
+            }
+        }
+    }
 }
