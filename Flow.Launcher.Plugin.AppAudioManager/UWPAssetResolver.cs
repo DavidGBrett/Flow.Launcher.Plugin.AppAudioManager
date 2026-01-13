09385e607892227228e5b12abbbad4248622018e
David Brett <davidbrett2002@gmail.com>
2026-01-13 21:32:24 +0100

    add GetQualifiersFromFilePath

--- Flow.Launcher.Plugin.AppAudioManager/UWPAssetResolver.cs
+++ Flow.Launcher.Plugin.AppAudioManager/UWPAssetResolver.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text.RegularExpressions;
 
 namespace Flow.Launcher.Plugin.AppAudioManager
@@ -19,6 +21,29 @@ namespace Flow.Launcher.Plugin.AppAudioManager
             var matches = Directory.GetFiles(directory, baseName + ".*" + extension, SearchOption.AllDirectories);
 
             return matches.ToList();
+        }
+
+        public static List<string> GetQualifiersFromFilePath(string basePath, string filePath)
+        {
+            var qualifiedPath = filePath.Substring(basePath.Length);
+            
+            // get qualifiers from the dir names before the file, eg  /qualifier-value/fileName.extension
+            IEnumerable<string> dirQualifiers = Path
+                .GetDirectoryName(qualifiedPath)
+                .TrimEnd(Path.DirectorySeparatorChar)
+                .Split(Path.DirectorySeparatorChar, StringSplitOptions.RemoveEmptyEntries)
+                .SelectMany((s)=>s.Split("_"));
+
+            // get qualifiers from the file name eg basename.qualifier-value_qualifier-value.extension
+            IEnumerable<string> fileNameQualifiers = Path
+                .GetFileNameWithoutExtension(qualifiedPath)
+                .Split(".")
+                .ElementAtOrDefault(1)
+                ?.Split("_")
+                ?? Array.Empty<string>();
+
+            return fileNameQualifiers.Concat(dirQualifiers).ToList();
+
         }
 
     }
