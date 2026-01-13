9e7c7c42ba47ea8ffd7cb63edf6459d205b576d5
David Brett <davidbrett2002@gmail.com>
2026-01-13 08:17:42 +0100

    Get UWP name from Properties.DisplayName and Icon from Properties.Logo

--- Flow.Launcher.Plugin.AppAudioManager/AudioSession.cs
+++ Flow.Launcher.Plugin.AppAudioManager/AudioSession.cs
@@ -1,5 +1,9 @@
 using System;
 using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
 using Microsoft.VisualBasic.Devices;
 using NAudio.CoreAudioApi;
 using NAudio.CoreAudioApi.Interfaces;
@@ -89,9 +93,70 @@ namespace Flow.Launcher.Plugin.AppAudioManager
             {
                 referenceProcess = parentProcess;
             }
-            Name = GetBestName(_session, referenceProcess);
+
             ProcessFilePath = GetProcessFilePath(process: referenceProcess);
-            IconPath = GetIconPath(_session, ProcessFilePath);
+
+            // if this is an UWP app
+            if (ProcessFilePath.StartsWith(
+                "C:\\Program Files\\WindowsApps\\"
+                // Environment.ExpandEnvironmentVariables("%SystemRoot%\\Program Files\\WindowsApps\\")
+            )){
+                var startIndex = ProcessFilePath.IndexOf("WindowsApps\\") + "WindowsApps\\".Length;
+                var appFolderPath = ProcessFilePath.Substring(0,ProcessFilePath.IndexOf("\\", startIndex) + 1);
+
+                string manifestPath = Path.Combine(appFolderPath, "AppxManifest.xml");
+                try
+                {
+                    XDocument doc = XDocument.Load(manifestPath);
+                    
+                    // Get the DisplayName using XML namespaces
+                    XNamespace defaultNamespace = doc.Root.GetDefaultNamespace();
+                    
+                    Name = doc.Root?
+                        .Element(defaultNamespace + "Properties")?
+                        .Element(defaultNamespace + "DisplayName")?
+                        .Value;
+
+                    if (string.IsNullOrEmpty(Name))
+                    {
+                        // Try without namespace as fallback
+                        Name = doc.Root?
+                            .Element("Properties")?
+                            .Element("DisplayName")?
+                            .Value;
+                    }
+
+                    string logoManifestPath = Path.Combine(
+                        appFolderPath,
+                        doc.Root?
+                        .Element(defaultNamespace + "Properties")?
+                        .Element(defaultNamespace + "Logo")?
+                        .Value
+                    );
+
+                    string logoBaseName = Path.GetFileNameWithoutExtension(logoManifestPath);
+                    string logoExtension = Path.GetExtension(logoManifestPath);
+                    string logoFolder = Path.GetDirectoryName(logoManifestPath);
+
+                    int[] scales = { 400, 200, 150, 125, 100 };
+
+                    string logoBestScaledPath = scales
+                    .Select(scale => Path.Combine(logoFolder, $"{logoBaseName}.scale-{scale}{logoExtension}"))
+                    .FirstOrDefault(File.Exists, null);
+
+                    IconPath = logoBestScaledPath ?? logoManifestPath;
+                    
+                } catch (Exception){}
+            }
+            
+            // if not UWP app
+            else
+            {
+                Name = GetBestName(_session, referenceProcess);
+            
+                IconPath = GetIconPath(_session, ProcessFilePath);
+            }
+            
 
             if (sessionProcess is not null) sessionProcess.Dispose();
             if (parentProcess is not null) parentProcess.Dispose();
